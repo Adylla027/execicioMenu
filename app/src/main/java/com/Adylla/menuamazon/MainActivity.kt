0f7e8d2e9a508900893ad6f8d18248e64c0894f6
adylla027 <adyllamoreiralopesmoreira@gmail.com>
2025-09-12 08:52:43 -0300

    menu em andamento

--- app/src/main/java/com/Adylla/menuamazon/MainActivity.kt
+++ app/src/main/java/com/Adylla/menuamazon/MainActivity.kt
@@ -2,19 +2,22 @@ package com.Adylla.menuamazon
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.Adylla.menuamazon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private  lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        toggle = ActionBarDrawerToggle(this,binding.drawerLayout,binding.toolbar,)
     }
 }
